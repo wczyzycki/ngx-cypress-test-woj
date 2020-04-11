66eed6f1887a14fef510b60a5041a9a77c3cde8e
Wojtek Czyżycki <wojtek.czyzycki@outlook.com>
2020-04-11 17:36:19 +0200

    lesson 16

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -265,5 +265,41 @@ describe('Our first suite', () => {
     })
   });
 
+  it('Datepicker dynamic date', () => {
+
+     const selectDayFromCurrent = day => {
+      let date = new Date();
+      date.setDate((date.getDate() + day));
+
+      let futureDay = date.getDate();
+      let futureMonth = date.toLocaleString('en-GB', {month: 'short'})
+      cy.log(futureMonth);
+
+      let dayAssert = futureMonth + ' ' + futureDay + ', ' + date.getFullYear();
+
+      cy.get('nb-calendar-navigation').invoke('attr', 'ng-reflect-date').then(dateAttribute => {
+          if (!dateAttribute.includes(futureMonth)) {
+            cy.get('[data-name="chevron-right"]').click();
+            selectDayFromCurrent(day);
+          } else {
+            cy.get('nb-calendar-day-picker [class="day-cell ng-star-inserted"]').contains(futureDay).click()
+          }
+        }
+      );
+      return dayAssert;
+    }
+
+    cy.visit('/');
+    cy.contains('Forms').click();
+    cy.contains('Datepicker').click();
+
+    cy.contains('nb-card', 'Common Datepicker').find('input').then(input => {
+      cy.wrap(input).click();
+      let dayAssert = selectDayFromCurrent(25);
+      cy.wrap(input).invoke('prop', 'value').should('contain', dayAssert);
+    });
+
+  });
+
 });
 
